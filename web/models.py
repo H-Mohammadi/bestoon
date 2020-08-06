9484356b27d84be0c53d87725ac072411a9f2054
victor <mohammadih120@gmail.com>
2020-08-06 10:25:16 +0430

    'register'

--- web/models.py
+++ web/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Passwordresetcodes(models.Model):
+    code = models.CharField(max_length=32)
+    email = models.CharField(max_length=120)
+    time = models.DateTimeField()
+    username = models.CharField(max_length=50)
+
+
 class Token(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=48)
