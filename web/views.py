a59f4cbbfb98555d82deb6c1f42c8c00b06f1c36
victor <mohammadih120@gmail.com>
2020-08-09 17:48:19 +0430

    'Recaptcha'

--- web/views.py
+++ web/views.py
@@ -6,18 +6,36 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import User, Token, Income, Expense
 from datetime import datetime
-
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+# random_str = lambda N: ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits)
 
 
-@csrf_exempt
 def register(request):
-    return JsonResponse({
-        'status': 'ok'
-    }, encoder=json.JSONEncoder)
+    return render(request, 'web/register.html')
+    # if request.POST.has_key('requestcode'):
+    #     if not grecaptcha_verify(request):
+    #         context = {'massage': "plz checked I`m not robot"}
+    #         return render(request, 'register.html', context)
+    #     if User.objects.filter(email=request.POST['email']).exists():
+    #         context = {'massage': 'email existed!'}
+    #         return render(request, 'register.html', context)
+    #     if not User.objects.filter(username=request.POST['username']).exists():
+    #         code = random_str(28)
+    #         now = datetime.now()
+    #         email = request.POST['email']
+    #         temporarycode = Passwordresetcodes(email=email, time=now, code=code, username=username, password=pass)
+    #         temporarycode.save()
+    #         message = PMMail(api_key=settings.POSTMARK_API_TOKEN,
+    #                          subject = "active account",
+    #                          sender= "mohammadih120@gmail",
+    #                          to= email,
+    #                          text_body = "for active your account click this link")
 
-# #csrf_cancel
+
+# csrf_cancel
 @csrf_exempt
 def submit_expense(request):
     """user submit request"""
@@ -54,6 +72,3 @@ def submit_income(request):
     return JsonResponse({
         'status': 'ok'
     }, encoder=json.JSONEncoder)
-
-
-
